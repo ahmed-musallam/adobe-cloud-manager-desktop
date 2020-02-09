a3fcde922684c251610d8f0bfe5ae2772d4fcf8f
Ahmed Musallam <atyaf66@gmail.com>
2020-02-09 00:49:26 -0600

    add tsconfig and global declarations

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,23 @@
+import VueRouter, { Route } from 'vue-router'
+
 // declare global variables
 declare const electronStore;
 declare const adobeAuth;
 declare const keytar;
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $router: VueRouter
+    on: FunctionStringCallback
+    $showLoadingScreen: Function
+    $hideLoadingScreen: Function
+  }
+  interface CoralEvent extends Event {
+    detail: {
+      selection: HTMLElement
+    };
+  }
+  interface CoralElement extends HTMLElement {
+    on(name: string, handler:(e: CoralEvent) => void): void
+  }
+}
