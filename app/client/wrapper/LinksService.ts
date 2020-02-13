e8486110e99ece876b5dfb336c8a8d96ff6a5937
Ahmed Musallam <atyaf66@gmail.com>
2020-02-13 01:46:43 -0600

    use base path in links service

--- app/client/wrapper/LinksService.ts
+++ app/client/wrapper/LinksService.ts
@@ -1,8 +1,8 @@
 import { BaseService } from "./BaseService";
 import { HalLink } from "../typescript-axios";
 import globalAxios from "axios";
+import { BASE_PATH } from "../typescript-axios/base";
 const UriTemplate = require("uritemplate");
-
 
 interface TemplateParams {
   [key: string]: string;
@@ -12,14 +12,14 @@ interface TemplateParams {
  * Service for requesting HalLinks that adobe did not document ¯\_(ツ)_/¯
  */
 export default class LinksService extends BaseService<undefined> {
-  getLink(link: HalLink, templateParams: TemplateParams) {
+  getLink(link: HalLink, templateParams?: TemplateParams) {
     let href = String(link?.href);
     if (link?.templated) {
       var template = UriTemplate.parse(href);
-      href = template.expand(templateParams)
+      href = template.expand(templateParams);
     }
-    console.log("[LinksService] getting href: ", href)
-    return globalAxios.get(String(href), {
+    console.log("[LinksService] getting href: ", href);
+    return globalAxios.get(BASE_PATH + String(href), {
       headers: {
         "Content-Type": "application/json",
         "x-api-key": this.apiKey,
