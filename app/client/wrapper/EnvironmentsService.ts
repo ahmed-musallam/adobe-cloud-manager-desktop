b29c03b59b4939a19c3cd7c80f093fbb618a0d44
Ahmed Musallam <atyaf66@gmail.com>
2020-02-21 14:16:54 -0600

    add new environment service methods

--- app/client/wrapper/EnvironmentsService.ts
+++ app/client/wrapper/EnvironmentsService.ts
@@ -1,21 +1,64 @@
 import { BaseService } from "./BaseService";
 import { EnvironmentsApi } from "../typescript-axios";
 export default class EnvironmentsService extends BaseService<EnvironmentsApi> {
-  getEnvironment(programId: string, environmentId: string) {
+  getEnvironment(programId: string, environmentId: string, options?: any) {
     return this.wrappedApi.getEnvironment(
       programId,
       environmentId,
       this.orgId,
       this.authorization,
-      this.apiKey
+      this.apiKey,
+      options
     );
   }
-  getEnvironments(programId: string) {
+  getEnvironments(programId: string, options?: any) {
     return this.wrappedApi.getEnvironments(
       programId,
       this.orgId,
       this.authorization,
-      this.apiKey
+      this.apiKey,
+      options
+    );
+  }
+  getEnvironmentLogs(
+    programId: string,
+    environmentId: string,
+    days: number,
+    service?: string[],
+    name?: string[],
+    options?: any
+  ) {
+    return this.wrappedApi.getEnvironmentLogs(
+      programId,
+      environmentId,
+      days,
+      this.orgId,
+      this.authorization,
+      this.apiKey,
+      service,
+      name
+    );
+  }
+  downloadLogs(
+    programId: string,
+    environmentId: string,
+    service: string,
+    name: string,
+    date: string,
+    Accept?: string,
+    options?: any
+  ) {
+    return this.wrappedApi.downloadLogs(
+      programId,
+      environmentId,
+      service,
+      name,
+      date,
+      this.orgId,
+      this.authorization,
+      this.apiKey,
+      Accept,
+      options
     );
   }
 }
