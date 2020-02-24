3994b56ab99ada4dd0ed7b3341756f32427f8707
Ahmed Musallam <atyaf66@gmail.com>
2020-02-24 09:11:47 -0600

    refactor and generate white .ico for windows
    original color does not look great on windows.

--- icon/generate-icns.sh
+++ icon/generate-icns.sh
@@ -8,26 +8,33 @@ sizes=(
   512x512
 )
 
+# Generate renditions at the sizes in "sizes" above, put all in ICONSET_FOLDER
 mkdir -p $ICONSET_FOLDER
 for size in "${sizes[@]}"; do
   icon="icon_${size}.png"
   ICON_FILES="$ICON_FILES $ICONSET_FOLDER/$icon"
-  echo Generating $icon
+  echo Generating $ICONSET_FOLDER/$icon
   convert $PNG_MASTER -quality 100 -resize $size $ICONSET_FOLDER/$icon
   
   icon="icon_${size}@2x.png"
   ICON_FILES="$ICON_FILES $ICONSET_FOLDER/$icon"
-  echo Generating $icon
+  echo Generating $ICONSET_FOLDER/$icon
   convert $PNG_MASTER -quality 100 -resize $size $ICONSET_FOLDER/$icon
 done
 
-
+# generate icon.icns for mac app (this only works on mac)
+echo Generating icon.icns
 iconutil -c icns $ICONSET_FOLDER -o icon.icns
+
+# Generate .ico file for windows
 ICON_FILES=""
 for size in "${sizes[@]}"; do
   ICON_FILES="$ICON_FILES $ICONSET_FOLDER/icon_${size}.png"
   ICON_FILES="$ICON_FILES $ICONSET_FOLDER/icon_${size}@2x.png"
 done
 echo Generating icon.ico
-convert $ICON_FILES icon.ico
+convert $ICON_FILES -colorize 100,100,100 icon.ico 
+
+# remove generated renditions
+echo removing $ICONSET_FOLDER folder
 rm -rf $ICONSET_FOLDER
