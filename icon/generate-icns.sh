88c99721f07efee1f940a7779c5d30f9f89cdf1f
Ahmed Musallam <atyaf66@gmail.com>
2020-02-23 23:11:54 -0600

    generate windows icon

--- icon/generate-icns.sh
+++ icon/generate-icns.sh
@@ -9,16 +9,25 @@ sizes=(
 )
 
 mkdir -p $ICONSET_FOLDER
-
 for size in "${sizes[@]}"; do
   icon="icon_${size}.png"
+  ICON_FILES="$ICON_FILES $ICONSET_FOLDER/$icon"
   echo Generating $icon
   convert $PNG_MASTER -quality 100 -resize $size $ICONSET_FOLDER/$icon
   
   icon="icon_${size}@2x.png"
+  ICON_FILES="$ICON_FILES $ICONSET_FOLDER/$icon"
   echo Generating $icon
   convert $PNG_MASTER -quality 100 -resize $size $ICONSET_FOLDER/$icon
 done
 
+
 iconutil -c icns $ICONSET_FOLDER -o icon.icns
+ICON_FILES=""
+for size in "${sizes[@]}"; do
+  ICON_FILES="$ICON_FILES $ICONSET_FOLDER/icon_${size}.png"
+  ICON_FILES="$ICON_FILES $ICONSET_FOLDER/icon_${size}@2x.png"
+done
+echo Generating icon.ico
+convert $ICON_FILES icon.ico
 rm -rf $ICONSET_FOLDER
