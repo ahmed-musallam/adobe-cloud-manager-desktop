677001052a9e4217b02dbd4b63753de3f48de325
Ahmed Musallam <atyaf66@gmail.com>
2020-03-17 22:13:47 -0500

    upgrade typescript and add electron store

--- preload.js
+++ preload.js
@@ -2,3 +2,5 @@
 window.adobeAuth = require("@adobe/jwt-auth");
 window.keytar = require("keytar");
 window.electron = require("electron");
+const Store = require("electron-store");
+window.electronStore = new Store();
